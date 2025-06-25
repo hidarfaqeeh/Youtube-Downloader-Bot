2c1f895b9092d058f898e0836740abb8526dcbf8
hidarfaqeeh <hidarfaqeeh@gmail.com>
2025-06-25 03:06:02 +0300

    Update ytdlfunc.py

--- helper/ytdlfunc.py
+++ helper/ytdlfunc.py
@@ -1,12 +1,10 @@
-from __future__ import unicode_literals
-from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, InlineKeyboardMarkup
-import youtube_dl
+from pyrogram.types import InlineKeyboardButton
 from utils.util import humanbytes
+from yt_dlp import YoutubeDL
 import asyncio
 
-
 def buttonmap(item):
-    quality = item['format']
+    quality = item["format"]
     if "audio" in quality:
         return [InlineKeyboardButton(f"{quality} 🎵 {humanbytes(item['filesize'])}",
                                      callback_data=f"ytdata||audio||{item['format_id']}||{item['yturl']}")]
@@ -14,65 +12,45 @@ def buttonmap(item):
         return [InlineKeyboardButton(f"{quality} 📹 {humanbytes(item['filesize'])}",
                                      callback_data=f"ytdata||video||{item['format_id']}||{item['yturl']}")]
 
-# Return a array of Buttons
-def create_buttons(quailitylist):
-    return map(buttonmap, quailitylist)
+def create_buttons(quality_list):
+    return map(buttonmap, quality_list)
 
-
-# extract Youtube info
 def extractYt(yturl):
-    ydl = youtube_dl.YoutubeDL()
+    ydl = YoutubeDL()
     with ydl:
         qualityList = []
         r = ydl.extract_info(yturl, download=False)
-        for format in r['formats']:
-            # Filter dash video(without audio)
-            if not "dash" in str(format['format']).lower():
-                qualityList.append(
-                {"format": format['format'], "filesize": format['filesize'], "format_id": format['format_id'],
-                 "yturl": yturl})
-
-        return r['title'], r['thumbnail'], qualityList
-
-
-#  Need to work on progress
-
-# def downloadyt(url, fmid, custom_progress):
-#     ydl_opts = {
-#         'format': f"{fmid}+bestaudio",
-#         "outtmpl": "test+.%(ext)s",
-#         'noplaylist': True,
-#         'progress_hooks': [custom_progress],
-#     }
-#     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-#         ydl.download([url])
-
-
-# https://github.com/SpEcHiDe/AnyDLBot
+        for format in r["formats"]:
+            if not "dash" in str(format["format"]).lower():
+                qualityList.append({
+                    "format": format["format"],
+                    "filesize": format.get("filesize", 0),
+                    "format_id": format["format_id"],
+                    "yturl": yturl
+                })
+        return r["title"], r["thumbnail"], qualityList
 
 async def downloadvideocli(command_to_exec):
     process = await asyncio.create_subprocess_exec(
         *command_to_exec,
-        # stdout must a pipe to be accessible as process.stdout
         stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE, )
+        stderr=asyncio.subprocess.PIPE
+    )
     stdout, stderr = await process.communicate()
     e_response = stderr.decode().strip()
     t_response = stdout.decode().strip()
     print(e_response)
-    filename = t_response.split("Merging formats into")[-1].split('"')[1]
+    filename = t_response.split('Merging formats into')[-1].split('"')[1]
     return filename
-
 
 async def downloadaudiocli(command_to_exec):
     process = await asyncio.create_subprocess_exec(
         *command_to_exec,
-        # stdout must a pipe to be accessible as process.stdout
         stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE, )
+        stderr=asyncio.subprocess.PIPE
+    )
     stdout, stderr = await process.communicate()
     e_response = stderr.decode().strip()
     t_response = stdout.decode().strip()
     print("Download error:", e_response)
-
     return t_response.split("Destination")[-1].split("Deleting")[0].split(":")[-1].strip()

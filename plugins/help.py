b02cdbb31c28c8d613df14edd331fbeb9596a66a
hidarfaqeeh <hidarfaqeeh@gmail.com>
2025-06-25 03:05:28 +0300

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -1,7 +1,6 @@
 from pyrogram import Client, filters
 
-
 @Client.on_message(filters.command(["help"]))
-async def start(client, message):
-    helptxt = f"Currently Only supports Youtube Single  (No playlist) Just Send Youtube Url"
+async def help_command(client, message):
+    helptxt = "البوت يدعم تحميل فيديو واحد فقط من يوتيوب (لا يدعم قائمة التشغيل). فقط أرسل رابط يوتيوب!"
     await message.reply_text(helptxt)

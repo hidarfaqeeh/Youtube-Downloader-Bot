b1805567ce885a265d56f6dc5bab13707215bc2a
hidarfaqeeh <hidarfaqeeh@gmail.com>
2025-06-25 03:06:37 +0300

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,22 +1,21 @@
 from pyrogram import Client
 import config
 
-DOWNLOAD_LOCATION = "./Downloads"
+DOWNLOAD_LOCATION = "./downloads"
 BOT_TOKEN = config.BOT_TOKEN
-
 APP_ID = config.APP_ID
 API_HASH = config.API_HASH
 
+plugins = dict(root="plugins")
 
-plugins = dict(
-    root="plugins",
-)
-
-Client(
+app = Client(
     "YouTubeDlBot",
     bot_token=BOT_TOKEN,
     api_id=APP_ID,
     api_hash=API_HASH,
     plugins=plugins,
     workers=100
-).run()
+)
+
+if __name__ == "__main__":
+    app.run()

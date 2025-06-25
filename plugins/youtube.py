44d4ea7634b8a33bb78b326d1415ee7004a61f53
hidarfaqeeh <hidarfaqeeh@gmail.com>
2025-06-25 03:04:05 +0300

    Update youtube.py

--- plugins/youtube.py
+++ plugins/youtube.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timedelta
-from pyrogram import Client, filters, InlineKeyboardMarkup, InlineKeyboardButton
-from bot import user_time
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup
 from config import youtube_next_fetch
 from helper.ytdlfunc import extractYt, create_buttons
 import wget
@@ -8,43 +8,38 @@ import os
 from PIL import Image
 
 ytregex = r"^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$"
-
+user_time = {}
 
 @Client.on_message(filters.regex(ytregex))
-async def ytdl(_, message):
+async def ytdl(client, message):
     userLastDownloadTime = user_time.get(message.chat.id)
     try:
-        if userLastDownloadTime > datetime.now():
+        if userLastDownloadTime and userLastDownloadTime > datetime.now():
             wait_time = round((userLastDownloadTime - datetime.now()).total_seconds() / 60, 2)
-            await message.reply_text(f"`Wait {wait_time} Minutes before next Request`")
+            await message.reply_text(f"`انتظر {wait_time} دقيقة قبل الطلب القادم`")
             return
-    except:
+    except Exception:
         pass
 
     url = message.text.strip()
     await message.reply_chat_action("typing")
     try:
         title, thumbnail_url, formats = extractYt(url)
-
         now = datetime.now()
-        user_time[message.chat.id] = now + \
-                                     timedelta(minutes=youtube_next_fetch)
-
+        user_time[message.chat.id] = now + timedelta(minutes=youtube_next_fetch)
     except Exception:
-        await message.reply_text("`Failed To Fetch Youtube Data... 😔 \nPossible Youtube Blocked server ip \n#error`")
+        await message.reply_text("`فشل في جلب بيانات اليوتيوب... 😔\nربما تم حظر اليوتيوب على الخادم\n#error`")
         return
     buttons = InlineKeyboardMarkup(list(create_buttons(formats)))
-    sentm = await message.reply_text("Processing Youtube Url 🔎 🔎 🔎")
+    sentm = await message.reply_text("جارٍ معالجة الرابط 🔎🔎🔎")
     try:
-        # Todo add webp image support in thumbnail by default not supported by pyrogram
-        # https://www.youtube.com/watch?v=lTTajzrSkCw
         img = wget.download(thumbnail_url)
         im = Image.open(img).convert("RGB")
         output_directory = os.path.join(os.getcwd(), "downloads", str(message.chat.id))
         if not os.path.isdir(output_directory):
             os.makedirs(output_directory)
         thumb_image_path = f"{output_directory}.jpg"
-        im.save(thumb_image_path,"jpeg")
+        im.save(thumb_image_path, "jpeg")
         await message.reply_photo(thumb_image_path, caption=title, reply_markup=buttons)
         await sentm.delete()
     except Exception as e:
@@ -53,6 +48,4 @@ async def ytdl(_, message):
             thumbnail_url = "https://telegra.ph/file/ce37f8203e1903feed544.png"
             await message.reply_photo(thumbnail_url, caption=title, reply_markup=buttons)
         except Exception as e:
-            await sentm.edit(
-            f"<code>{e}</code> #Error")
-
+            await sentm.edit(f"<code>{e}</code> #Error")
